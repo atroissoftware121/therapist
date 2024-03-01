4a0ee5359302d9cea66468759c2d1f03226a24b1
vishal90909 <testmytest129@gmail.com>
2024-03-01 10:56:56 +0530

    add changes

--- src/controllers/commonController.js
+++ src/controllers/commonController.js
@@ -229,15 +229,12 @@ const getProfile = async (req, res) => {
 
 const startSession = (io) => async (req, res) => {
   const { individualId, therapistsId } = req.query;
-  console.log('therapistsId', therapistId);
   const [userChatDetails] = await findQuery(chatDetailsModel,
     {
       receiverId: therapistsId,
       'individualDetails.senderId': { $eq: individualId }
     });
     
-    console.log('therapistsId122', therapistId);
-    console.log('userChatDetails', userChatDetails);
 
   if (!userChatDetails) {
     return SendBadResponse({
@@ -257,7 +254,6 @@ const startSession = (io) => async (req, res) => {
     sessionId: createSession._id,
     startSession: sessionStartTime,
   };
-  console.log('=========', therapistId)
   io.emit('startTimer', data);
 
   await updateQuery(chatDetailsModel,
